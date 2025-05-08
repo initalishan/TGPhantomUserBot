7487ca6babf2cdaa399fd0a1e347ef61c26edcb2
initalishan <initalishan@gmail.com>
2025-05-08 17:48:34 +0530

    Create ping plugin

--- phantom/__main__.py
+++ phantom/__main__.py
@@ -1,5 +1,18 @@
 from phantom.core.clients import phantom
-from phantom.plugins import ping
+from os import listdir
+import importlib
+from asyncio import get_event_loop
 
+async def main():
+    import_plugins()
+    print("Phantom UserBot started.")
+    await phantom.run_until_disconnected()
 
-phantom.run_until_disconnected()
+def import_plugins():
+    path = "phantom/plugins"
+    for file in listdir(path):
+        if file.endswith(".py") and not file.startswith("__"):
+            importlib.import_module(f"phantom.plugins.{file[:-3]}")
+            print(f"{file[:-3]} plugin loaded succesfully.")
+
+get_event_loop().run_until_complete(main())
--- phantom/plugins/ping.py
+++ phantom/plugins/ping.py
@@ -2,6 +2,9 @@ from phantom.core.clients import phantom
 from telethon import events
 from time import time
 import socket
+from datetime import datetime
+
+start_time = datetime.now()
 
 async def get_ping(host="8.8.8.8", port=53, timeout=3):
     try:
@@ -11,15 +14,18 @@ async def get_ping(host="8.8.8.8", port=53, timeout=3):
         sock.connect((host, port))
         end = time()
         sock.close()
-        ping_time = round((end - start) * 1000, 2)
+        ping_time = int(round((end - start) * 1000, 2))
         return f"{ping_time}ms"
     except socket.error:
         return "Ping failed"
     
 @phantom.on(events.NewMessage(outgoing=True, pattern=r"\.ping"))
 async def ping_handler(event):
-    latency = time()
-    await event.edit("Pinging..")
-    latency = time()
+    start = time()
+    await event.edit("🏓 Pinging..")
+    end = time()
     ping = await get_ping()
-    await event.edit(f"Pong!\nPing: {ping}\nLatency: {latency}")
+    latency = int((end - start) * 1000)
+    uptime = (datetime.now() - start_time)
+    uptime_str = str(uptime).split(".")[0]
+    await event.edit(f"🏓 Pong!\nPing: {ping}\nLatency: {latency}ms\nUptime: {uptime_str}")
